3993d2efe762d15faef3415a3f5a3cf448844cb8
NNN1590 <nnn1590@nnn1590.org>
2021-08-03 17:06:52 +0900

    libmsstyle: Add build.sh

---
+++ libmsstyle/build.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -eu +f
+
+trap "kill 0" EXIT
+
+rm -f *.dll *.exe *.o *.so *.a *.specs
+rm -rf build/
+
+#[[ -f 'updlist/updlist.vcproj' ]] && cd updlist
+
+declare SPECS=''
+declare _OUT=''
+declare _SUFFIX="-win32"
+declare _IFS_BACKUP="${IFS}"
+
+for _PREFIX in 'x86_64-w64-mingw32-' 'i686-w64-mingw32-'; do
+	[[ "${_PREFIX}" == 'x86_64-w64-mingw32-' ]] && continue  # Skip x86_64 build
+	#[[ "${_PREFIX}" == 'i686-w64-mingw32-' ]] && continue  # Skip x86 build
+	"${_PREFIX}g++${_SUFFIX}" -dumpspecs | sed -e 's/-lmsvcrt/-lmsvcr120/g' > "${_PREFIX}g++${_SUFFIX}.specs"
+	set +u
+	if [[ ! "x${1}X" == "xX" ]]; then
+		SPECS='-specs='"${_PREFIX}g++${_SUFFIX}.specs"
+	fi
+	set -u
+	IFS=$'\n'
+	declare -a _FILES=($(grep 'ClCompile' libmsstyle.vcxproj | grep -o '".*"' | sed -e 's/"//g'))
+	declare -a _FILES_OBJ=("${_FILES[@]/.cpp/.o}")
+	IFS=''
+	for i in "${_FILES[@]}"; do
+		"${_PREFIX}g++${_SUFFIX}" -c ${SPECS} -D_WIN32_WINNT=0x0601 -DWINVER=0x0601 -std=c++11 -mwindows -D_CRT_SECURE_NO_WARNINGS -D_LIB -DUNICODE -D_UNICODE -municode -static-libgcc -static-libstdc++ "${i}" -Wno-narrowing &
+	done
+	wait
+	"${_PREFIX}g++${_SUFFIX}" -o libmsstyle.dll ${SPECS} -D_WIN32_WINNT=0x0601 -DWINVER=0x0601 -std=c++11 -mwindows -D_CRT_SECURE_NO_WARNINGS -D_LIB -DUNICODE -D_UNICODE -municode -shared -static-libgcc -static-libstdc++ "${_FILES_OBJ[@]}" -Wno-narrowing
+	"${_PREFIX}ar" r libmsstyle.a "${_FILES_OBJ[@]}"
+	rm *.o
+	mkdir build
+	[[ "${_PREFIX}" == 'x86_64-w64-mingw32-' ]] && _OUT="build/x86_64${_SUFFIX}" || _OUT="build/x86${_SUFFIX}"
+	mkdir -v "${_OUT}"
+	mv -v libmsstyle.{a,dll} "${_OUT}"
+	IFS="${_IFS_BACKUP}"
+done
